bea7ae1b06458386905867f55d7c11f94a150fd1
ilickona0 <ilickona@gmail.com>
2020-10-20 13:23:31 -0400

    Added for loop to try and create score sums over time

--- Sports_Graph.R
+++ Sports_Graph.R
@@ -1,15 +1,28 @@
 #Script to analyze and graph sports game data
 
 #Load data into R
-rawdata <- read.csv("UWvMSU_1-22-13.txt", header= TRUE, stringsAsFactors = FALSE)
+rawdata <- read.table("UWvMSU_1-22-13.txt", header= TRUE, stringsAsFactors = FALSE)
 
-#Initialize variables
+#Initialize variables, make table appropriate number of colums
 table <- data.frame()
+table[1,1]<- 0
+table[1,2] <- 0
+table[1,3]<- 0
+colnames(table)<- c("Time","UW Score","MSU Score")
 
+for (i in 1:nrow(rawdata)){
+  table[i,1]<- rawdata[i,1]
+  if (rawdata[i,2]=="UW"){
+    if (i == 1){
+      table[i,2]<-rawdata[i,3]
+    }else {table[i,2]<- (table[(i-1),2]+rawdata[i,3])
+    }
+  }
+  else (rawdata[i,2]== "MSU")
+        if (i == 1){
+          table[i,3]<-rawdata[i,3]
+        }else {table[i,3]<- (table[(i-1),3]+rawdata[i,3])
+        }
+}
 
-
-
-for (i in rawdata){
-  if rawdata[i,2]=="UW"
-  table[i,1] <- rawdata[i,3]
   
