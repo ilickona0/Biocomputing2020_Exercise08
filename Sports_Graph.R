01f505bb3c6bbe9dcbf331c6606619a64dd318bf
ilickona0 <ilickona@gmail.com>
2020-10-20 12:49:11 -0400

    Started loop to make cumulative score data frame

--- Sports_Graph.R
+++ Sports_Graph.R
@@ -1,4 +1,15 @@
 #Script to analyze and graph sports game data
 
-rawdata <- read.table("UWvMSU_1-22-13.txt")
+#Load data into R
+rawdata <- read.csv("UWvMSU_1-22-13.txt", header= TRUE, stringsAsFactors = FALSE)
 
+#Initialize variables
+table <- data.frame()
+
+
+
+
+for (i in rawdata){
+  if rawdata[i,2]=="UW"
+  table[i,1] <- rawdata[i,3]
+  
